2c82d598f8b0604cc3e706c76950bcdd6eebf7a0
Rohith <rpalem@drcloudehr.com>
2022-11-05 07:30:42 +0530

    1

--- app/app.ts
+++ app/app.ts
@@ -2,7 +2,7 @@ import { svelteNativeNoFrame } from 'svelte-native';
 import App from './App.svelte';
 import { Http } from '@nativescript/core'
 
-Http.getJSON('http://127.0.0.1:8000/test').then(
+Http.request('http://127.0.0.1:8000/fetchGuestToken').then(
   (result: any) => {
     console.log(result)
   },
