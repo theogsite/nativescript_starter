a81b0c6f910ba1f24f2db875271af245e5179282
Rohith <rpalem@drcloudehr.com>
2022-11-05 07:17:22 +0530

    1

--- app/app.ts
+++ app/app.ts
@@ -1,4 +1,12 @@
 import { svelteNativeNoFrame } from 'svelte-native';
 import App from './App.svelte';
+import { Http } from '@nativescript/core'
+
+Http.getJSON('https://httpbin.org/get').then(
+  (result: any) => {
+    console.log(result)
+  },
+  e => {}
+)
 
 svelteNativeNoFrame(App, {});
